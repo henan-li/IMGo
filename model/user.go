02e1b5a88eb8f71a22ef3295e7d668d82cc9def3
nathan <nathan@riverpayment.com>
2020-02-07 12:04:41 -0800

    html files

--- model/user.go
+++ model/user.go
@@ -2,15 +2,26 @@ package model
 
 import "time"
 
+const (
+	SEX_WOMEN = "W"
+	SEX_MEN   = "M"
+	//
+	SEX_UNKNOW = "U"
+)
+
 type User struct {
-	Id       int64     `json:"id" form:"id" xorm:"pk autoincr bigint(20)"`
-	Mobile   string    `json:"mobile" form:"mobile" xorm:"varchar(40)"`
-	Passwd   string    `json:"passwd" form:"passwd" xorm:"varchar(40)"`
-	Avatar   string    `json:"avatar" form:"avatar" xorm:"varchar(150)"`
-	NickName string    `json:"nick_name" form:"nick_name" xorm:"varchar(40)"`
-	Salt     string    `json:"salt" form:"salt" xorm:"varchar(40)"`
-	Online   int       `json:"online" form:"online" xorm:"int(10)"`
-	Token    string    `json:"token" form:"token" xorm:"varchar(40)"`
-	Memo     string    `json:"memo" form:"memo" xorm:"varchar(150)"`
-	Creatat  time.Time `json:"creatat" form:"creatat" xorm:"datetime"`
+	//用户ID
+	Id       int64  `xorm:"pk autoincr bigint(20)" form:"id" json:"id"`
+	Mobile   string `xorm:"varchar(20)" form:"mobile" json:"mobile"`
+	Passwd   string `xorm:"varchar(40)" form:"passwd" json:"-"` // 什么角色
+	Avatar   string `xorm:"varchar(150)" form:"avatar" json:"avatar"`
+	Sex      string `xorm:"varchar(2)" form:"sex" json:"sex"`            // 什么角色
+	Nickname string `xorm:"varchar(20)" form:"nickname" json:"nickname"` // 什么角色
+	//加盐随机字符串6
+	Salt   string `xorm:"varchar(10)" form:"salt" json:"-"`    // 什么角色
+	Online int    `xorm:"int(10)" form:"online" json:"online"` //是否在线
+	//前端鉴权因子,
+	Token    string    `xorm:"varchar(40)" form:"token" json:"token"`    // 什么角色
+	Memo     string    `xorm:"varchar(140)" form:"memo" json:"memo"`     // 什么角色
+	Createat time.Time `xorm:"datetime" form:"createat" json:"createat"` // 什么角色
 }
